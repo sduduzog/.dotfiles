71e96485e12e7a036792dfa346b393e53c43d1e1
Sdu <48126337+sduduzog@users.noreply.github.com>
2026-04-10 16:53:56 +0200

    chore(nvim): remove Comment.nvim and ts_context_commentstring

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,12 +2,6 @@ return {
   'nvim-treesitter/nvim-treesitter',
   dependencies = {
     { 'nvim-treesitter/nvim-treesitter-textobjects', branch = 'main' },
-    {
-      'JoosepAlviste/nvim-ts-context-commentstring',
-      opts = {
-        enable_autocmd = false,
-      },
-    },
     'nvim-treesitter/nvim-treesitter-context',
   },
   build = ':TSUpdate',
